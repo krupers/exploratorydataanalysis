8ed6269821a9d1d97ad571b6c80ca22a0d29dcc9
Artur <akcorporation@gmail.com>
2015-03-02 19:22:45 +0100

    Update plot4.r

--- plot4.r
+++ plot4.r
@@ -22,15 +22,15 @@ par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
 with(data, {
   plot(Global_active_power~Datetime,
        type="l",
-       ylab="Global Active Power (kilowatts)",
+       ylab="Global Active Power",
        xlab="")
   plot(Voltage~Datetime,
        type="l",
-       ylab="Voltage (volt)",
+       ylab="Voltage",
        xlab="")
   plot(Sub_metering_1~Datetime,
        type="l",
-       ylab="Global Active Power (kilowatts)",
+       ylab="Energy sub metering",
        xlab="")
   lines(Sub_metering_2~Datetime,col='Red')
   lines(Sub_metering_3~Datetime,col='Blue')
@@ -42,7 +42,7 @@ with(data, {
          legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
   plot(Global_reactive_power~Datetime,
        type="l",
-       ylab="Global Rective Power (kilowatts)",
+       ylab="Global Rective Power",
        xlab="")
 })
 
